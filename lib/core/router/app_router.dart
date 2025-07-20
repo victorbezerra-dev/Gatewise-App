b88ed690ff1b9021477f303a12784b225454a310
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-20 14:14:04 -0400

    feat(router): configure GoRouter with login route

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,14 @@
+import 'package:gatewise_app/modules/authenticate/presentation/auth_screen.dart';
 import 'package:go_router/go_router.dart';
 import '../../modules/splash_screen/presentation/splash_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
-  routes: [GoRoute(path: '/', builder: (context, state) => const SplashPage())],
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const SplashPage()),
+    GoRoute(
+      path: '/auth-login',
+      builder: (context, state) => const AuthLoginScreen(),
+    ),
+  ],
 );
