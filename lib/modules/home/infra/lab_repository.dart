303388266268555d9f1e6e395952e411260bbbf0
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-21 21:15:29 -0400

    feat: add method to fetch access grants to logged user

--- lib/modules/home/infra/lab_repository.dart
+++ lib/modules/home/infra/lab_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import '../../../core/domain/entities/user_entity.dart';
 import '../../../core/domain/interfaces/custom_http_client.dart';
 import '../../../core/infra/secure_storage.dart';
+import '../domain/entities/access_grant.dart';
 import '../domain/entities/open_lab_request.dart';
 
 class LabRepository {
@@ -25,6 +27,31 @@ class LabRepository {
 
     if (response.statusCode != 200) {
       throw Exception('Failed to open lab: ${response.body}');
+    }
+  }
+
+  Future<List<AccessGrant>> getAccessGrants() async {
+    final accessToken = await SecureStore.accessToken;
+    final userJson = await SecureStore.getUserJson();
+
+    if (userJson == null) {
+      throw Exception('An error occurred while retrieving access permissions');
+    }
+
+    final userMap = jsonDecode(userJson) as Map<String, dynamic>;
+    final user = User.fromJson(userMap);
+
+    if (accessToken == null) {
+      throw Exception('Access token not found');
+    }
+
+    final response = await httpClient.get('/api/AccessGrants/user/${user.id}');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = jsonDecode(response.body);
+      return jsonList.map((json) => AccessGrant.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to fetch access grants: ${response.body}');
     }
   }
 }
